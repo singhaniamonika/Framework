ddb7196a049c637de055d7e870b9143ffa8ec5ff
Monika Singhania <monikalib67@gmail.com>
2019-12-30 20:36:05 +0530

    Try to commit

--- CucumberPractise/src/test/java/runner/TestRunner.java
+++ CucumberPractise/src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
+//Runner file used to run the cucumber feature file
 
 @RunWith(Cucumber.class)  
 @CucumberOptions(features="features",glue= "stepDefinition",
